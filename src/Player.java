afc93cd7e63788d796f36fc92d6591d6080c2547
Tom Peng <XPeng@bayviewglen.ca>
2026-04-16 09:29:36 -0400

    games1

--- src/Player.java
+++ src/Player.java
@@ -10,18 +10,40 @@ public class Player {
     private Gear helmet;
     private Gear bodyArmor;
 
-    private Weapon pistol;   
-    private Weapon rifle;    
-    private boolean usingRifle; 
+    private Weapon pistol;
+    private Weapon rifle;
+    private boolean usingRifle;
 
     private int foodCount, waterCount, medkitCount;
     private int grenades;
     private int kills;
     private int level;
-    private int medkit;
 
-    
-    private double lootBonusPerLevel;    
-    private double accuracyBonusPerLevel; 
+    private double lootBonusPerLevel;
+    private double accuracyBonusPerLevel;
 
-   
+    public Player(String name, int health, int hunger, int energy) {
+        this.name = name;
+        this.maxHealth = health;
+        this.health = health;
+        this.maxHunger = hunger;
+        this.hunger = hunger;
+        this.maxEnergy = energy;
+        this.energy = energy;
+
+        this.helmet = Gear.createHelmet();
+        this.bodyArmor = Gear.createBodyArmor();
+
+        this.pistol = Weapon.createPistol();
+        this.rifle = null;
+        this.usingRifle = false;
+
+        this.foodCount = 3;
+        this.waterCount = 3;
+        this.medkitCount = 1;
+        this.grenades = 3;
+        this.kills = 0;
+        this.level = 1;
+
+    }
+}
