c8e9e1e71cf5090f5206136166e9b1453a6b7dcf
Tom Peng <XPeng@bayviewglen.ca>
2026-04-16 13:12:53 -0400

    New changes

--- src/Weapon.java
+++ src/Weapon.java
@@ -1,12 +1,27 @@
 public class Weapon {
-    private String id;
     private String name;
-    private String description;
+    private String type; // "pistol" or "rifle"
+    private double hitChance;
+    private double headshotChance;
+    private int damage;
+    private int headshotDamage;
+    private int magSize; // rounds per magazine
+    private int currentAmmo; // rounds in current mag
+    private int reserveMags; // extra magazines carried
+    private boolean fullAuto;
 
-    public Weapon(String id, String name, String description) {
-        this.id = id;
+    public Weapon(String name, String type, double hitChance, double headshotChance, int damage, int headshotDamage,
+            int magSize, int startMags, boolean fullAuto) {
         this.name = name;
-        this.description = description;
+        this.type = type;
+        this.hitChance = hitChance;
+        this.headshotChance = headshotChance;
+        this.damage = damage;
+        this.headshotDamage = headshotDamage;
+        this.magSize = magSize;
+        this.currentAmmo = magSize; // starts with a full mag loaded
+        this.reserveMags = startMags;
+        this.fullAuto = fullAuto;
     }
 
     public String getId() {
