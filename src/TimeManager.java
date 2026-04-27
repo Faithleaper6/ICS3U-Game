67d850dd39bbe78b7a5e4917e77c5428a8f5d3e9
Tom Peng <XPeng@bayviewglen.ca>
2026-04-27 09:39:27 -0400

    1.1.6 - Updated TimeManager Class

--- src/TimeManager.java
+++ src/TimeManager.java
@@ -1,3 +1,31 @@
+import java.util.*;
+
 public class TimeManager {
+    private String[] times = { "Morning", "Noon", "Afternoon", "Night" };
+    private int currentTimeIndex;
+    private int dayCount;
+    private int difficultyLevel;
+
+    public TimeManager(int difficultyLevel) {
+        this.currentTimeIndex = 0; // Start at Morning
+        this.dayCount = 1;
+        this.difficultyLevel = difficultyLevel;
+    }
+
+    public String getCurrentTime() {
+        return times[currentTimeIndex];
+    }
+
+    public int getDayCount() {
+        return dayCount;
+    }
+
+    public void advanceTime(Player player, ArrayList<Enemy> enemies) {
+        currentTimeIndex = (currentTimeIndex + 1) % times.length;
+        if (currentTimeIndex == 0) {
+            dayCount++;
+            System.out.println("\n--- Day " + dayCount + " ---");
+        }
+    }
 
 }
