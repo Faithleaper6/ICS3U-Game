1b680bd76478e1cfa4ea505172ff428abee064bb
Tom Peng <XPeng@bayviewglen.ca>
2026-04-27 09:45:41 -0400

    1.1.9 - Updated TimeManager class with hunger and energy drain

--- src/TimeManager.java
+++ src/TimeManager.java
@@ -20,12 +20,21 @@ public class TimeManager {
         return dayCount;
     }
 
-    public void advanceTime(Player player, ArrayList<Enemy> enemies) {
+    public void advanceTime(Player player, ArrayList<Enemy> enemies, ArrayList<Boolean> allies) {
         currentTimeIndex = (currentTimeIndex + 1) % times.length;
         if (currentTimeIndex == 0) {
             dayCount++;
             System.out.println("\n--- Day " + dayCount + " ---");
         }
+        System.out.println("\nTime advances to: " + getCurrentTime());
+
+        int hungerDrain = 5 + difficultyLevel * 2 * (Math.random() < 0.5 ? 1 : 2);
+        int energyDrain = 5 + difficultyLevel * 2 * (Math.random() < 0.5 ? 1 : 2);
+
+        if (getCurrentTime().equals("Night")) {
+            energyDrain += 5;
+        }
+
     }
 
 }
